f63ca22993f3b3de7df063fdbf36f452ae329f2e
rumroses12 <grace@daher.net>
2018-09-21 17:29:21 -0400

    almost'

--- router/wikiRouter.js
+++ router/wikiRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 module.exports = router;
 const addPage = require('../views/addPage');
-const main = require('../views/main')
+const main = require('../views/main');
 
 router.get('/', (req, res, next) => {
 
@@ -18,8 +18,25 @@ router.get('/add', (req, res, next) => {
     res.send(addPage())
 })
 
-router.post('/', (req, res, next) => {
-    console.log('req', req.body);
+
+router.post('/', async (req, res, next) => {
+    // STUDENT ASSIGNMENT:
+  // add definitions for `title` and `content`
+
+  const page = new Page({
+    title: req.body.title,
+    content: req.body.content,
+    slug: req.body.title.split(' ').join('_'),
+    status: req.body.status
+  });
+  console.log('this worked!',page);
+  res.send();
+  // make sure we only redirect *after* our save is complete!
+  // note: `.save` returns a promise.
+  try {
+    await page.save();
+    res.redirect('/');
+  } catch (error) { next(error) }
 
 })
 
