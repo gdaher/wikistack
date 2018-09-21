66564c66ebec4bc29abdd5abbac4d86d79fc828c
rumroses12 <grace@daher.net>
2018-09-21 12:35:53 -0400

    added db and models

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const morgan = require('morgan');
 const express = require('express')
 const app = express();
 const bodyParser = require('body-parser');
-const html = require('./views/main')
+const html = require('./views/main');
+const { db,Page,User } = require('./models');//destructuring assignment. 
 
 app.use(morgan('dev'));
 app.use(express.static('public'))
@@ -12,6 +13,12 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.get('/', (req, res, next) => {
     res.send(html());
 })
+
+db.authenticate().
+then(() => {
+  console.log('connected to the database');
+});
+
 
 const PORT = 3000;
 
